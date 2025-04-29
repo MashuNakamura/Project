4cef5870c4ff50ec7422188e1f1c877c1611ff88
Okerachuchupurin <145318409+MashuNakamura@users.noreply.github.com>
2025-04-30 00:05:48 +0700

    Update Circular Queue

--- python/Struktur Data/Week_3/circular_queue.py
+++ python/Struktur Data/Week_3/circular_queue.py
@@ -2,41 +2,47 @@ class CircularQueue:
     def __init__(self, size):
         self.size = size
         self.queue = [None] * size
-        self.front = self.rear = -1
+        self.front = self.rear = - 1
+
+    def is_empty(self):
+        return self.front == - 1
+    
+    def is_full(self):
+        return (self.rear + 1) % self.size == self.front
 
     def enqueue(self, data):
-        if (self.rear + 1) % self.size == self.front:
+        if self.is_full():
             print("Queue Penuh!")
             return
-        if self.front == -1:
+        
+        if self.front == - 1:
             self.front = self.rear = 0
         else:
             self.rear = (self.rear + 1) % self.size
         self.queue[self.rear] = data
 
     def dequeue(self):
-        if self.front == -1:
+        if self.is_empty():
             print("Queue Kosong!")
-            return
+            return None
+        
         removed = self.queue[self.front]
+
         if self.front == self.rear:
-            self.front = self.rear = -1
+            self.front = self.rear = - 1
         else:
             self.front = (self.front + 1) % self.size
         return removed
 
     def display(self):
-        if self.front == -1:
-            print("Queue Kosong!")
-            return
+        result = []
         i = self.front
-        print("Isi Queue :", end=" ")
         while True:
-            print(self.queue[i], end=" ")
+            result.append(self.queue[i])
             if i == self.rear:
                 break
             i = (i + 1) % self.size
-        print()
+        print(f"Queue : {result}")
 
 if __name__ == "__main__":
     size = int(input("Masukkan ukuran queue : "))
@@ -54,13 +60,18 @@ if __name__ == "__main__":
         if pilihan == "1":
             data = input("Masukkan data yang ingin ditambahkan : ")
             cq.enqueue(data)
+
         elif pilihan == "2":
             removed = cq.dequeue()
-            print(f"Data yang dihapus: {removed}")
+            if removed is not None:
+                print(f"Data yang dihapus: {removed}")
+
         elif pilihan == "3":
             cq.display()
+
         elif pilihan == "4":
             print("Tuhan Memberkati.")
             break
+        
         else:
             print("Invalid Menu")
