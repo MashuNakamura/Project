3f09dde0e7d0efb5100241978d17fce36db173b4
Okerachuchupurin <145318409+MashuNakamura@users.noreply.github.com>
2025-04-30 14:31:54 +0700

    Circular Queue Final

--- python/Struktur Data/Week_3/Circular_Queue/circular_queue.py
+++ python/Struktur Data/Week_3/Circular_Queue/circular_queue.py
@@ -1,12 +1,17 @@
+import random
+
 class CircularQueue:
     def __init__(self, size):
         self.size = size
         self.queue = [None] * size
         self.front = self.rear = - 1
+        self.random_pos = random.randint(0, self.size - 1)
+        self.current_pos = self.random_pos
+        self.is_first = False
 
     def is_empty(self):
-        return self.front == - 1
-    
+        return self.front == -1
+
     def is_full(self):
         return (self.rear + 1) % self.size == self.front
 
@@ -15,31 +20,39 @@ class CircularQueue:
             print("Queue Penuh!")
             return
         
-        if self.front == - 1:
-            self.front = self.rear = 0
+        elif self.is_empty() and self.is_first == False:
+            self.front = self.rear = self.random_pos
+            self.is_first = True
+        elif self.is_empty() and self.is_first == True:
+            self.front = self.rear = self.current_pos
         else:
             self.rear = (self.rear + 1) % self.size
+        
         self.queue[self.rear] = data
 
     def dequeue(self):
         if self.is_empty():
             print("Queue Kosong!")
             return None
-        
+
         removed = self.queue[self.front]
+        self.queue[self.front] = None
 
         if self.front == self.rear:
             self.front = self.rear = - 1
         else:
             self.front = (self.front + 1) % self.size
+
         return removed
 
     def display(self):
-        result = []
-        i = self.front
-        while True:
-            result.append(self.queue[i])
-            if i == self.rear:
-                break
-            i = (i + 1) % self.size
-        print(f"Queue : {result}")
+        visual = []
+
+        for item in self.queue:
+            if item is None:
+                visual.append("None")
+            else:
+                visual.append(str(item))
+
+        result = ", ".join(visual)
+        print("Queue  :", f"[{result}]")
